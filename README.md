3e59aead4762ab2c838d30f503b509d0fb9c8c59
luka-casey <luka.casey@gmail.com>
2025-01-06 22:49:31 +1100

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-Creating a personalised investment portfolio app that tracks stockmarket data
-https://luka-casey.github.io/InvestmentTracker/
--- src/helpers/getTotalChangePercent.js
+++
@@ -1,7 +0,0 @@
-function getTotalChangePercent(currentMarketPrice, originalMarketPrice) {
-    const totalChangePercent = ((currentMarketPrice - originalMarketPrice) / originalMarketPrice) * 100;
-    return totalChangePercent.toFixed(2)
-}
-
-
-export default getTotalChangePercent;
